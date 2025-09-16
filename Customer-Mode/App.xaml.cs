5bbf53e008905e707912a8f7b205c698b4d54b27
Christian Ramirez <christianrc7325@gmail.com>
2025-09-16 11:47:15 -0500

    added more

---
+++ Customer-Mode/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Customer_Mode
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
